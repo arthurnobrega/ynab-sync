d4d91c707c2ddb827b0f85e590c59df4014d28f4
Pedro Lauro <pedro.lauro@gmail.com>
2018-05-05 12:46:58 -0300

    Main menu; favorite listing; favorite deleting

--- src/db.js
+++ src/db.js
@@ -4,7 +4,15 @@ import FileSync from 'lowdb/adapters/FileSync'
 const adapter = new FileSync('db.json')
 const db = lowdb(adapter)
 
-db.defaults({ nubankTokens: [] })
+db.defaults({
+  nubankTokens: [],
+  favoriteOperations: [
+    { id: 1, name: '01030433143' },
+    { id: 2, name: '00429229178' },
+    { id: 3, name: '333333333' },
+    { id: 4, name: '4444444444' },
+  ],
+})
   .write()
 
 export default db
--- src/index.js
+++ src/index.js
@@ -1,21 +1,110 @@
 import chalk from 'chalk'
+import inquirer from 'inquirer'
 import executeYnabFlow from './ynab'
 import executeNubankFlow from './nubank'
+import db from './db'
 
-async function main(options) {
-  if (!process.env.YNAB_TOKEN) {
-    console.log(chalk.red('You need to set YNAB_TOKEN environment variable. Please read the README.md.'))
-    return
-  }
+async function askForFavoriteOpsToRun() {
+  const favoriteOpsDb = db.get('favoriteOperations').value()
+  const { favoriteOps } = await inquirer.prompt([{
+    type: 'checkbox',
+    name: 'favoriteOps',
+    message: 'Which favorite operations would you like to run?',
+    choices: favoriteOpsDb.map(op => ({
+      value: op.id,
+      checked: true,
+      name: op.name,
+    })),
+  }])
+  console.log(chalk.green(`favOps ${favoriteOps}`))
+  return favoriteOps
+}
 
+async function askForFavoriteOpsToDelete() {
+  const favoriteOpsDb = db.get('favoriteOperations').value()
+  const { favoriteOps } = await inquirer.prompt([{
+    type: 'checkbox',
+    name: 'favoriteOps',
+    message: 'Which favorite operations would you like to DELETE?',
+    choices: favoriteOpsDb.map(op => ({
+      value: op.id,
+      name: op.name,
+    })),
+  }])
+
+  favoriteOps.forEach((id) => {
+    db.get('favoriteOperations')
+      .remove({ id })
+      .write()
+  })
+}
+
+async function executeSync() {
   try {
     const transactions = await executeNubankFlow()
     await executeYnabFlow(transactions)
   } catch (e) {
     console.log(chalk.red(e.toString()))
-
-    return null
   }
+}
+
+async function askForOperationType() {
+  const { operationType } = await inquirer.prompt([{
+    type: 'list',
+    name: 'operationType',
+    message: 'What would you like to do?',
+    choices: [
+      {
+        value: 'NEW',
+        name: 'Start a new operation',
+      },
+      {
+        value: 'FAVORITE',
+        name: 'Use favorite operations',
+      },
+      {
+        value: 'DELETE',
+        name: 'Delete favorite operations',
+      },
+      {
+        value: 'EXIT',
+        name: 'Exit',
+      },
+    ],
+  }])
+  return operationType
+}
+
+async function main() {
+  if (!process.env.YNAB_TOKEN) {
+    console.log(chalk.red('You need to set YNAB_TOKEN environment variable. Please read the README.md.'))
+    return
+  }
+
+  let endWhile = false;
+  while (!endWhile) {
+    const opType = await askForOperationType()
+    switch (opType) {
+      case 'NEW':
+        await executeSync()
+        endWhile = true
+        break
+
+      case 'FAVORITE':
+        await askForFavoriteOpsToRun()
+        endWhile = true
+        break
+
+      case 'DELETE':
+        await askForFavoriteOpsToDelete()
+        break
+
+      default:
+        endWhile = true
+        break
+    }
+  }
+
 }
 
 main()
